bc0c87bb3367de2d926b1a8288f94186a1c3ac70
tan-si-jing <tansijing@yahoo.com.sg>
2024-02-19 23:56:00 +0800

    connect to database

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -81,6 +81,20 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'LittleLemon',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
